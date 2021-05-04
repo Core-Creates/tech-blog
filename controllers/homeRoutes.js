fceb941c917d59a78a2364f1bc545bbcd11f4de9
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-05-03 23:17:34 -0500

    edited routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { User } = require('../models');
-// const withAuth = require('../utils/auth');
+// const { User } = require('../models');
 
-router.get('/', withAuth, async (req, res) => {
+
+router.get('/', async (req, res) => {
   try {
     res.render('homepage', { });
 
